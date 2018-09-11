f7addd9368fedac2217dd9090c2cf36a1163b484
sane5ever <sane5ever@gmail.com>
2018-09-11 16:47:45 +0400

    1.3. JavaDoc. Code convention(#4087)

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+ * Calculate.
+ *  
+ * @author Alexander Savchenko (sane5ever@gmail.com)
+ * @since 11.09.2018
+ */
+
 public class Calculate {
 	
+	/**
+	  *Main.
+	  *@param args - args.
+	  */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *Package for calculate task9
+ *
+ *@author Alexander Savchenko (sane5ever@gmail.com)
+ *@version 1.0
+ *@since 11.09.2018
+ */
+package ru.job4j.calculate;
