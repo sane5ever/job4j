2ba9dfdb3c93c081bb83d87cc5ec3ecbfb99b1a1
sane5ever <sane5ever@gmail.com>
2018-09-14 15:33:08 +0400

    4.3. Вычисление площади треугольника [#9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,73 @@
+package ru.job4j.condition;
+
+/**
+ * Вычисление площади треугольника.
+ * @author Alexander Savchenko (sane5ever@gmail.com)
+ * @version 1.0
+ * @since 2018-09-14
+ */
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    /**
+     *
+     * @param a первая вершина треугольника
+     * @param b вторая вершина треугольника
+     * @param c третья вершина треугольника
+     */
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисляет полупериметр треугольника по длинам сторон.
+     *
+     * Формула: (ab + ac + bc) / 2
+     *
+     * @param ab расстояние между точками a и b
+     * @param ac расстояние между точками a и c
+     * @param bc расстояние между точками b и c
+     * @return полупериметр
+     */
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника по формуле Герона.
+     *
+     * @return площадь, если треугольник существует, иначе -1
+     */
+    public double area() {
+        double result = -1D;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            result = Math.sqrt(
+                    p * (p - ab) * (p - ac) * (p - bc)
+            );
+        }
+        return result;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     * Треугольник существует если сумма любых двух сторон больше третьей.
+     *
+     * @param ab расстояние между точками a и b
+     * @param ac расстояние между точками a и c
+     * @param bc расстояние между точками b и c
+     * @return true, если существует, иначе false
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc)
+                && (ab + bc > ac)
+                && (ac + bc > ab);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест-класс для Triangle
+ * @author Alexander Savchenko (sane5ever@gmail.com)
+ * @version 1.0
+ * @since 2018-09-14
+ */
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+
+        double expected = 2D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    @Test
+    public void whenTriangleDoesNotExist() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 5);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+
+        double expected = -1D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
