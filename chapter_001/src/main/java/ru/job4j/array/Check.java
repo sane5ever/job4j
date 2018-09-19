ae1d03515b8279b65fe2affdeac451a1c84c3d75
sane5ever <sane5ever@gmail.com>
2018-09-19 12:37:41 +0400

    6.3. Массив заполнен true или false [#53857]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * 6.3. Массив заполнен true или false.
+ *
+ * @author Alexander Savchenko (sane5ever@gmail.com)
+ * @version 1.0
+ * @since 2018-09-19
+ */
+public class Check {
+    /**
+     *
+     * @param data входящие массив c boolean-значениями
+     * @return true, если все эл-ты массива идентичны, иначе false
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        boolean checker = data[0];
+        for (int index = 1; index < data.length; index++) {
+            if (checker != data[index]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Тест-класс для Check.
+ *
+ * @author Alexander Savchenko (sane5ever@gmail.com)
+ * @version 1.0
+ * @since 2018-09-19
+ */
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
