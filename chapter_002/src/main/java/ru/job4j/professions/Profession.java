f8f9a14a837833bb7f484a3ce1a3846ba1edaac8
sane5ever <sane5ever@gmail.com>
2018-10-02 18:18:55 +0400

    1. Реализация профессий в коде [#6837]

---
+++ chapter_002/src/main/java/ru/job4j/professions/Profession.java
@@ -0,0 +1,14 @@
+package ru.job4j.professions;
+
+public class Profession {
+    String name;
+    String profession;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getProfession() {
+        return this.profession;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/professions/Teacher.java
@@ -0,0 +1,8 @@
+package ru.job4j.professions;
+
+public class Teacher extends Profession {
+
+    public void teach(Student student) {
+    }
+
+}
