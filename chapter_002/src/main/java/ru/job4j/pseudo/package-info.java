35857e6f51d551c0c951f649f6910ef80bfe373c
sane5ever <sane5ever@gmail.com>
2018-10-06 14:36:18 +0400

    --откат--

--- chapter_002/src/main/java/ru/job4j/pseudo/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Пакет классов, реализующих вывод в консоль фигур в псевдографике.
- *
- *@author Alexander Savchenko
- *@version 1.0
- *@since 2018-10-04
- */
-package ru.job4j.pseudo;
--- chapter_002/src/test/java/ru/job4j/pseudo/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Пакет классов, тестирующих отрисовку в консоль фигур в псевдографике.
- *
- * @author Alexander Savchenko
- * @version 1.0
- * @since 2018-10-06
- */
-package ru.job4j.pseudo;
