052ee026b22c1429cc523c173caf756356249c05
sane5ever <sane5ever@gmail.com>
2018-10-08 13:53:10 +0400

    --откат--

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -3,30 +3,20 @@ package ru.job4j.tracker;
 /**
  * Расширенный класс для ввода пользовательских данных из консоли, ввод проверяется на соответствие логике программы.
  */
-public class ValidateInput implements Input {
-
-    private final Input input;
-
-    public ValidateInput(final Input input) {
-        this.input = input;
-    }
-
-    @Override
-    public String ask(String question) {
-        return this.input.ask(question);
-    }
-
+public class ValidateInput extends ConsoleInput {
     @Override
     public int ask(String question, int[] range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = this.input.ask(question, range);
+                value = super.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
+                moe.printStackTrace();
                 System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
                 System.out.println("Please enter valid data again.");
             }
         } while (invalid);
