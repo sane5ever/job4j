d5ba57ca1be071bbfc24199e18c0f69403032925
sane5ever <sane5ever@gmail.com>
2018-10-17 19:27:16 +0400

    --добавление недостающих package-info--

---
+++ chapter_003/src/main/java/ru/job4j/search/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Классы, реализующие поиск данных.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2018-10-16
+ */
+package ru.job4j.search;
---
+++ chapter_003/src/test/java/ru/job4j/search/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Класссы, тестирующие реализацию поиска данных.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2018-10-16
+ */
+package ru.job4j.search;
