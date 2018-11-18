f9d8c12dbaaecf3ed036eea4ebcfa98b2205fde8
sane5ever <sane5ever@gmail.com>
2018-11-18 20:29:21 +0400

    5.3.0 Создать метод delete для односвязного списка[#85684]

---
+++ chapter_004/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class SimpleArrayListTest {
+
+    private SimpleArrayList<Integer> list;
+
+    @Before
+    public void prepare() {
+        this.list = new SimpleArrayList<>();
+        this.list.add(1);
+        this.list.add(2);
+        this.list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(this.list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(this.list.getSize(), is(3));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseDeleteThenGetSizeResultTwo() {
+        this.list.delete();
+        assertThat(this.list.getSize(), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseDeleteTwoTimesThenGetZeroResultThree() {
+        this.list.delete();
+        this.list.delete();
+        assertThat(this.list.get(0), is(1));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseDeleteFourTimesThenResultNullAndSizeZero() {
+        this.list.delete();
+        this.list.delete();
+        this.list.delete();
+        assertThat(this.list.getSize(), is(0));
+        assertNull(this.list.delete());
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/list/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Пакет для выполнения заданий урока «List». Тесты.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2018-11-18
+ */
+package ru.job4j.list;
