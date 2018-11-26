0928c23bb96810ff602a9ba117f6f3b73b279105
sane5ever <sane5ever@gmail.com>
2018-11-26 17:29:03 +0400

    5.3.4. Задан связанный список. Определить цикличность.[#85689]{reinvented}

--- chapter_004/src/main/java/ru/job4j/list/LinkedListChecker.java
+++ chapter_004/src/main/java/ru/job4j/list/LinkedListChecker.java
@@ -2,14 +2,10 @@ package ru.job4j.list;
 
 /**
  * Оеспечивает работу алгоритма, определяющего, что связанный список содержит замыкания.
- * Список содержит замыкания, если встречается хотя бы один элемент, next-указатель которого
- * ссылается на элемент уже встречавшийся ранее в списке.
- * Без создания вспомогательных структур проверить это условие можно путём сравнения каждого next-указателя
- * последовательно со всеми элементами, кот. располагаются в списке, начиная с первого и до текущего включительно
- * (если элемент ссылается сам на себя).
+ * Используется алгоритм Флойда поиска цикла (Floyd's Tortoise and Hare).
  *
  * @author Alexander Savchenko
- * @version 1.0
+ * @version 1.01 (2018-11-26)
  * @since 2018-11-20
  */
 public class LinkedListChecker {
@@ -20,34 +16,13 @@ public class LinkedListChecker {
      * @return true, при нахождении первого же замыкания
      */
     public <T> boolean hasCycle(Node<T> first) {
-        boolean result = false;
-        int index = 1;
-        for (Node<T> cursor = first.next; cursor != null && !result; cursor = cursor.next, index++) {
-            result = this.isPresentBefore(first, cursor, index);
+        Node<T> basicStep = first.next;
+        Node<T> doubleStep = first.next != null ? first.next.next : null;
+        while (doubleStep != null && basicStep != doubleStep) {
+            basicStep = basicStep.next;
+            doubleStep = doubleStep.next != null ? doubleStep.next.next : null;
         }
-        return result;
-    }
-
-    /**
-     * Проверяет, совпадает ли указатель с каким-то элементом списка, идущим до него.
-     * Условие соблюдается, если указатель равен элементу, индекс кот. меньше индекса указателя.
-     *
-     * @param first   head-элемент списка
-     * @param current проверяемый указатель
-     * @param index   колво элементов, кот. необходимо проверить
-     * @return true, если элемент встречается ранее
-     */
-    private <T> boolean isPresentBefore(Node<T> first, Node<T> current, int index) {
-        boolean result = false;
-        Node<T> cursor = first;
-        for (int i = 0; i < index; i++) {
-            if (current == cursor) {
-                result = true;
-                break;
-            }
-            cursor = cursor.next;
-        }
-        return result;
+        return doubleStep != null;
     }
 }
 
