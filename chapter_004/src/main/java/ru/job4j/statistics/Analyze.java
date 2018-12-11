0feeb69d87b01acfec6671d3fc4b6238c8757d9c
sane5ever <sane5ever@gmail.com>
2018-12-11 23:17:27 +0400

    7.1. Статистика по коллекции.[#85680]{reinvented}

--- chapter_004/src/main/java/ru/job4j/statistics/Analyze.java
+++ chapter_004/src/main/java/ru/job4j/statistics/Analyze.java
@@ -1,7 +1,10 @@
 package ru.job4j.statistics;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Статистика по коллекции.
@@ -20,47 +23,21 @@ public class Analyze {
      */
     public Info diff(List<User> previous, List<User> current) {
         Info result = new Info();
-        result.deleted = this.count(previous, user -> this.hasNotMatchId(current, user.id));
-        result.added = this.count(current, user -> hasNotMatchId(previous, user.id));
-        result.changed = this.count(current, user -> hasMatchIdDiffName(previous, user.id, user.name));
+        Map<Integer, User> users = previous.stream().collect(
+                Collectors.toMap(
+                        user -> user.id,
+                        Function.identity())
+        );
+        current.forEach(user -> {
+            User temp = users.remove(user.id);
+            if (temp == null) {
+                result.added++;
+            } else if (!temp.name.equals(user.name)) {
+                result.changed++;
+            }
+        });
+        result.deleted = users.size();
         return result;
-    }
-
-    /**
-     * Возвращает количество элементов коллекции, удовлетворяющие переданному условию.
-     *
-     * @param users     переданная коллекция
-     * @param predicate условие
-     * @return количество элементов
-     */
-    private int count(List<User> users, Predicate<User> predicate) {
-        return users.stream()
-                .filter(predicate)
-                .mapToInt(el -> 1)
-                .reduce(0, Integer::sum);
-    }
-
-    /**
-     * Проверяет, отсутствует ли в коллекции элемент с заданным id.
-     *
-     * @param users переданная коллекция
-     * @param id    искомый id
-     * @return true, если элемент с заданым id не найден
-     */
-    private boolean hasNotMatchId(List<User> users, int id) {
-        return users.stream().noneMatch(user -> user.id == id);
-    }
-
-    /**
-     * Проверяет, присутствует ли в коллекции пользователь с заданным id, но отличным именем.
-     *
-     * @param users переданная коллекция
-     * @param id    искомый id
-     * @param name  имя
-     * @return true, если у пользователя с id иное имя
-     */
-    private boolean hasMatchIdDiffName(List<User> users, int id, String name) {
-        return users.stream().anyMatch(user -> user.id == id && !user.name.equals(name));
     }
 
     /**
