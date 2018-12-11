668fc90eef321ae07d7cb71a9c4b809c443f72bc
sane5ever <sane5ever@gmail.com>
2018-12-11 23:23:55 +0400

    --removing of redundant import--

--- chapter_004/src/main/java/ru/job4j/statistics/Analyze.java
+++ chapter_004/src/main/java/ru/job4j/statistics/Analyze.java
@@ -3,7 +3,6 @@ package ru.job4j.statistics;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
