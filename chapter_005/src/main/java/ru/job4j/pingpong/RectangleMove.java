b7e6e8b6843c4d0b22db680ee5911194f7ebdfcf
sane5ever <sane5ever@gmail.com>
2018-12-13 13:06:56 +0400

    1.1. Пин-понг.[#85764]

---
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,30 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private final int limitX;
+
+    public RectangleMove(Rectangle rect, int limitX) {
+        this.rect = rect;
+        this.limitX = limitX;
+    }
+
+    @Override
+    public void run() {
+        int dx = 1;
+        while (true) {
+            if (this.rect.getX() <= 0 || this.rect.getX() + this.rect.getWidth() >= this.limitX) {
+                dx = -dx;
+            }
+            this.rect.setX(this.rect.getX() + dx);
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/pingpong/StartUI.java
@@ -0,0 +1,7 @@
+package ru.job4j.pingpong;
+
+public class StartUI {
+    public static void main(String[] args) {
+        PingPong.main(args);
+    }
+}
