2e44eaecdd5030f96cfd8ee7203bb7ad64d6bc2c
sane5ever <sane5ever@gmail.com>
2019-02-08 19:57:12 +0400

    1.2. Удаление запрещенных слов [#859]

--- chapter_007/src/main/java/ru/job4j/io/InputChecker.java
+++ chapter_007/src/main/java/ru/job4j/io/InputChecker.java
@@ -3,8 +3,11 @@ package ru.job4j.io;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Проверка байтового потока.
@@ -15,6 +18,9 @@ import java.io.InputStream;
  */
 public class InputChecker {
     private static final Logger LOG = LogManager.getLogger(InputChecker.class);
+
+    private boolean firstLine;
+    private Pattern regex;
 
     /**
      * Метод проверяет, что в байтовом потоке записано чётное число
@@ -38,4 +44,96 @@ public class InputChecker {
         }
         return isDigitSequence && lastByte % 2 == 0;
     }
+
+    /**
+     * Метод удаляет запрещённые слова из потока ввода, записывая результат в выходной поток.
+     * Удаление осуществляется путём перекрытия слов звёздочками в том же количества, что и длина запрещённого слова.
+     *
+     * @param in    входной поток
+     * @param out   выходной поток
+     * @param abuse массив запрещённых слов
+     */
+    public void dropAbuses(InputStream in, OutputStream out, String[] abuse) {
+        firstLine = true;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+             PrintWriter writer = new PrintWriter(out)
+        ) {
+            reader.lines().map(getMapper(abuse)).forEach(writer::print);
+        } catch (Exception ioe) {
+            LOG.error(ioe.getMessage(), ioe);
+        }
+    }
+
+    /**
+     * Возвращает маппер строк на выходной поток.
+     * (в случае отсутствия запрещённых слов в списке никаких проверок производить не следует)
+     *
+     * @param abuse запрещённые слова
+     * @return маппер строк
+     */
+    private Function<String, String> getMapper(String[] abuse) {
+        return abuse == null || abuse.length == 0
+                ? this::prepareToOutput
+                : line -> dropAbuses(line, abuse);
+    }
+
+    /**
+     * Преобразует строки на выходной поток при отсутствии запрещённых слов.
+     * (необходимо исключительно для недопущения добавления дополнительного переноса строки в конце потока)
+     *
+     * @param line строка из входного потока
+     * @return идентичная строка при первом заходе, либо перенос строки + строка в остальных случаях
+     */
+    private String prepareToOutput(String line) {
+        String result;
+        if (firstLine) {
+            firstLine = false;
+            result = line;
+        } else {
+            result = String.format("%n%s", line);
+        }
+        return result;
+    }
+
+    /**
+     * Преобразует строки на выходной поток при наличии запрещённых слов в переданном массиве.
+     * Добавляет в начало перенос строки во все строки, кроме первой.
+     *
+     * @param line  строка из входного потока
+     * @param abuse список запрещённых слов
+     * @return строка с затёртыми запрещёнными словами.
+     */
+    private String dropAbuses(String line, String[] abuse) {
+        StringBuilder result = new StringBuilder();
+        if (firstLine) {
+            regex = Pattern.compile(getCensorWordsRegex(abuse), Pattern.CASE_INSENSITIVE);
+            firstLine = false;
+        } else {
+            result.append(System.lineSeparator());
+        }
+        return regex.matcher(result.append(line)).replaceAll("*");
+    }
+
+    /**
+     * Создаёт регулярное выражение на базе массива запрещённых слов.
+     * (?<=[].{0,N-1}.) — группа с положительной ретроспективной проверкой
+     * (захватываем для перекрытия все символы,
+     * последовательно соответствующие символам конкретному слова из массива)
+     * \\bWORD\\b — обеспечиваем границы слова, чтобы не затирались символы в словах,
+     * часть кот. совпадает с словом из списка
+     * («war»: stop the war -> stop the ***, но warehouse for rent -> warehouse for rent)
+     *
+     * @param words запрещённые слова
+     * @return регулярное выражение для фильтрации запрещённых слов
+     */
+    private String getCensorWordsRegex(String[] words) {
+        return Arrays.stream(words).map(
+                w -> String.format(
+                        "(?<=(?=\\b%s\\b).{0,%d}).",
+                        Pattern.quote(w),
+                        w.length() - 1
+                )
+        ).collect(Collectors.joining("|"));
+    }
+
 }
---
+++ chapter_007/src/test/java/ru/job4j/io/DropAbusesInputCheckerTest.java
@@ -0,0 +1,101 @@
+package ru.job4j.io;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Удаление запрещённых слов. Тестирование.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2019-02-08
+ */
+public class DropAbusesInputCheckerTest {
+    private static final Logger LOG = LogManager.getLogger(DropAbusesInputCheckerTest.class);
+    private static final String[] ABUSES = new String[]{
+            "damn",
+            "hell",
+            "crap",
+            "dammit",
+            "bitch"
+    };
+    private static final String[] EMPTY_ABUSES = new String[0];
+
+    private final InputChecker checker = new InputChecker();
+
+    @Test
+    public void whenSingleLineStreamThenAbusesShouldBeRemoved() {
+        String line = "hello bitch! it's hell out there";
+        String expected = "hello *****! it's **** out there";
+        onSingleLineStream(line, expected, ABUSES);
+        onSingleLineStream(line, line, EMPTY_ABUSES);
+        onSingleLineStream(line, line, null);
+    }
+
+    @Test
+    public void whenMultiLineStreamThenAbusesShouldBeRemoved() {
+        String lines = "hello it's hell out there%s oh god DAMMIT get out of hell%<s shame on me";
+        String expected = "hello it's **** out there%s oh god ****** get out of ****%<s shame on me";
+        onMultiLineStream(lines, expected, ABUSES);
+        onMultiLineStream(lines, lines, EMPTY_ABUSES);
+        onMultiLineStream(lines, lines, null);
+    }
+
+    @Test
+    public void whenEmptyStreamThenShouldBeEmptyStream() {
+        assertEquals("", execute("", ABUSES));
+        assertEquals("", execute("", EMPTY_ABUSES));
+        assertEquals("", execute("", null));
+    }
+
+    @Test
+    public void whenIOExceptionThrown() {
+        try (InputStream in = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                throw new IOException();
+            }
+        };
+             ByteArrayOutputStream out = new ByteArrayOutputStream()
+        ) {
+            checker.dropAbuses(in, out, EMPTY_ABUSES);
+            assertThat(out.size(), is(0));
+        } catch (IOException ignore) {
+        }
+    }
+
+    private void onSingleLineStream(String line, String expected, String[] abuses) {
+        String result = execute(line, abuses);
+        assertEquals(String.format(expected, System.lineSeparator()), result);
+    }
+
+    private void onMultiLineStream(String lines, String expected, String[] abuses) {
+        String result = execute(String.format(lines, System.lineSeparator()), abuses);
+        assertEquals(String.format(expected, System.lineSeparator()), result);
+    }
+
+    private String execute(String lines, String[] abuses) {
+        String result;
+        try (InputStream in = new ByteArrayInputStream(lines.getBytes());
+             ByteArrayOutputStream out = new ByteArrayOutputStream()
+        ) {
+            checker.dropAbuses(in, out, abuses);
+            result = out.toString();
+        } catch (IOException ioe) {
+            LOG.error(ioe.getMessage(), ioe);
+            result = null;
+        }
+        return result;
+    }
+
+}
